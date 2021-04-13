0fe4c72505e3ce1ee996490345807a89e51c1459
AdrianHolkLebinski <G00395620@gmit.ie>
2021-04-13 23:39:24 +0100

    Added a new user panel when logged in
    Now we have a proper user panel where you can check your account details when you are logged in

--- Website/Scripts/JavaScript/LoginDetectionDOM.js
+++ Website/Scripts/JavaScript/LoginDetectionDOM.js
@@ -1,12 +1,16 @@
-//Start of checking is user is logged in - Javascript (C.McG.)
+//Start of checking if user is logged in - Javascript (C.McG.)
 	//newButton is a HTML elemnt <div>
 		var newButton = document.createElement('div');
 	//newButton is a HTML elemnt <h5>
 		var welcomeMessage = document.createElement('h5');
 	//Get localStorage key 'Username' and set it to var
 		var localStorageUsername = localStorage.getItem('Username');
+		//Get localStorage key 'Password' and set it to var
+		var localStoragePassword = localStorage.getItem('Password');
 	//Grabbing the element logout will appear in
 		var x = document.getElementById("logout");
+	//Preventing each page from having an error bc theres no loginPage id
+		var loginPageCheck = document.getElementById('loginPage');
 			
 		var message = document.getElementById("welcomeMessage");
 			
@@ -28,6 +32,30 @@
 			
 			//adds a h1 element to the id welcomeMessage and adds a line of text to it.
 			message.append(welcomeMessage);
-			welcomeMessage.innerHTML = 'Welcome back, ' + localStorageUsername;
+			welcomeMessage.innerHTML = '<b>Welcome back, </b>' + localStorageUsername + "<b>!</b>";
+			
+			//changes login page to an information center for your profile (A.H-L)
+			if(document.getElementById('loginPage') != null)
+			{
+			let tempCoveredPassword = "";
+			for(var i = 1; i <= localStoragePassword.length;i++)
+			{
+				tempCoveredPassword += "*"
+			}
+			loginPage.innerHTML = "<h2> <br> <br>&emsp; &emsp; Username = " + localStorageUsername + " <br> <br> &emsp; &emsp; Password&nbsp; = <div style=\"display: inline;\"id=\"showPassword\">" + tempCoveredPassword + "</div> </h2> <br><h3>&emsp; &nbsp;&nbsp; &emsp;<input type=\"checkbox\" onclick=\"showPasswordAfterLogin()\">&nbsp;&nbsp;Show Password</h3>";
+			//Show Password	(C.McGr)
+			function showPasswordAfterLogin()
+			{
+				let x = document.getElementById("showPassword");
+				if (x.textContent == localStoragePassword)
+				{
+					x.textContent = tempCoveredPassword;
+				}
+				else 
+				{
+					x.textContent = localStoragePassword;
+				}
+				}
+			}
 		}
 //End of checking is user is logged in - Javascript
