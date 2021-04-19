7966fabfe46c5e15837d3e251042b5871fd8a7c9
CianMcGriskin <81272459+CianMcGriskin@users.noreply.github.com>
2021-04-19 15:24:15 +0100

    Updated the cart funtions

--- Website/Scripts/JavaScript/CartDOM.js
+++ Website/Scripts/JavaScript/CartDOM.js
@@ -1,79 +1,112 @@
 
 var customers = document.getElementById('customers');
 
-//Action
+//Game
 var CSGO = localStorage.getItem("01Game");
 var GTA5 = localStorage.getItem("02Game");
 var RS6 = localStorage.getItem("03Game");
 var L4D = localStorage.getItem("04Game");
 var warframe = localStorage.getItem("05Game");
 
-let price = localStorage.getItem("01Price");
-	//CSGO
+//Price
+var CSGOprice = localStorage.getItem("01Price");
+var GTAprice = localStorage.getItem("02Price");
+var RS6price = localStorage.getItem("03Price");
+var L4Dprice = localStorage.getItem("04Price");
+	
+//CSGO
 	if(CSGO == "Counter-Strike Global Offensive")
 	{
 		let name = CSGO;
-		customers.innerHTML+= '<tr>' + '<td><img id="thumbImages" src="GenreGames/Action/GameImages/CSGO.webp"></td>' + '<td>' + name +'</td>' + '<td>' + price + '</td>' + '<td><div id="product-quantity"><input type="number" id="quantityValue" value="1" min="1"></div> <button class="SaveButton" onclick="saveButton()">Save</button></td>' + '<td><button class="remove-product" onclick="removeCSGO()">Remove</button></td>' + '<td>'+ price + '</td></tr>';
+		
+		customers.innerHTML+= '<tr>' + '<td><img id="thumbImages" src="GenreGames/Action/GameImages/CSGO.webp"></td>' + '<td>' + name +'</td>' + '<td>' + CSGOprice + '</td>' + '<td><div id="product-quantity"><input type="number" id="quantityValue" value="1" min="1"></div> <button class="SaveButton" onclick="saveButtonCSGO()">Save</button></td>' + '<td><button class="remove-product" onclick="removeCSGO()">Remove</button></td>' + '<td id="totalPrice">' + CSGOprice + '</td></tr>';
 	}
 	
 	function removeCSGO()
-		{
-			localStorage.removeItem("01Game");
-			localStorage.removeItem("01Price");
-			location.reload();
-		}
-		
-	function saveButton()
-		{
-			let x = document.getElementById("quantityValue").value;
-			var totalPrice = price * x;
-			alert(totalPrice);
-			customers.innerHTML+= '<tr>' + '<td><img id="thumbImages" src="GenreGames/Action/GameImages/CSGO.webp"></td>' + '<td>' + name +'</td>' + '<td>' + price + '</td>' + '<td><div id="product-quantity"><input type="number" id="quantityValue" value="1" min="1"></div> <button class="SaveButton" onclick="saveButton()">Save</button></td>' + '<td><button class="remove-product" onclick="removeCSGO()">Remove</button></td>' + '<td>'+ totalPrice + '</td></tr>';
-		}
-		
-	//GTA5
-	if(GTA5 == "Grand Theft Auto V")
 	{
-		let name = GTA5;
-		let price = localStorage.getItem("02Price");
-		customers.innerHTML+= '<tr>' + '<td><img id="thumbImages" src="GenreGames/Action/GameImages/GTA.webp"></td>' + '<td>' + name +'</td>' + '<td>' + price + '</td>' + '<td><div id="product-quantity"><input type="number" id="quantityValue" value="1" min="1"></div></td>' + '<td><button class="remove-product" onclick="removeCSGO()">Remove</button></td>';
+		localStorage.removeItem("01Game");
+		localStorage.removeItem("01Price");
+		location.reload();
+	}
 		
-		function removeGTA()
-		{
-			localStorage.removeItem("02Game");
-			localStorage.removeItem("02Price");
-			location.reload();
-		}
-	}
-	
-	//RS6
-	if(RS6 == "Rainbow Six Siege")
+	function saveButtonCSGO()
 	{
-		let name = RS6;
-		let price = localStorage.getItem("03Price");
-		customers.innerHTML+= '<tr>' + '<td><img id="thumbImages" src="GenreGames/Action/GameImages/R6.webp"></td>' + '<td>' + name +'</td>' + '<td><div id="product-quantity"><input type="number" id="quantityValue" value="1" min="1"></div></td>' + '<td>' + price + '</td>' + '<td><button class="remove-product" onclick="removeRS6()">Remove</button></td>' + '</tr>';
-	
-		function removeRS6()
-		{
-			localStorage.removeItem("03Game");
-			localStorage.removeItem("03Price");
-			location.reload();
-		}
+		let totalPriceContent = document.getElementById("totalPrice");
+		let x = document.getElementById("quantityValue").value;
+		let totalPrice = CSGOprice * x;
+		totalPriceContent.textContent = totalPrice;
 	}
+//End of CSGO
+
+//GTA5
+if(GTA5 == "Grand Theft Auto V")
+{
+	let name = GTA5;
+	customers.innerHTML+= '<tr>' + '<td><img id="thumbImages" src="GenreGames/Action/GameImages/GTA.webp"></td>' + '<td>' + name +'</td>' + '<td>' + GTAprice + '</td>' + '<td><div id="product-quantity"><input type="number" id="quantityValueGTA" value="1" min="1"></div> <button class="SaveButton" onclick="saveButtonGTA()">Save</button></td>' + '<td><button class="remove-product" onclick="removeGTA()">Remove</button></td>' + '<td id="totalPriceGTA">' + GTAprice + '</td></tr>';
+}
+
+function removeGTA()
+{
+	localStorage.removeItem("02Game");
+	localStorage.removeItem("02Price");
+	location.reload();
+}
+
+function saveButtonGTA()
+{
+	let totalPriceContentGTA = document.getElementById("totalPriceGTA");
+	let x = document.getElementById("quantityValueGTA").value;
+	let totalPriceGTA = GTAprice * x;
+	totalPriceContentGTA.textContent = totalPriceGTA;
+}
+//End of GTA5
+	
+//RS6
+if(RS6 == "Rainbow Six Siege")
+{
+	let name = RS6
+	customers.innerHTML+= '<tr>' + '<td><img id="thumbImages" src="GenreGames/Action/GameImages/R6.webp"></td>' + '<td>' + name +'</td>' + '<td>' + RS6price + '</td>' + '<td><div id="product-quantity"><input type="number" id="quantityValueRS6" value="1" min="1"></div> <button class="SaveButton" onclick="saveButtonRS6()">Save</button></td>' + '<td><button class="remove-product" onclick="removeRS6()">Remove</button></td>' + '<td id="totalPriceRS6">' + RS6price + '</td></tr>';
+}
+
+function removeRS6()
+{
+	localStorage.removeItem("03Game");
+	localStorage.removeItem("03Price");
+	location.reload();
+}
+
+function saveButtonRS6()
+{
+	let totalPriceContentRS6 = document.getElementById("totalPriceRS6");
+	let x = document.getElementById("quantityValueRS6").value;
+	let totalPriceRS6 = RS6price * x;
+	totalPriceContentRS6.textContent = totalPriceRS6;
+}
+//End of RS6
 	
 	
-	if(L4D == "Left For Dead Two")
-	{
-		let name = L4D;
-		customers.innerHTML+= '<tr>' + '<td><img id="thumbImages" src="GenreGames/Action/GameImages/L4D.jpg"></td>' + '<td>' + name +'</td>' + '<td><div id="product-quantity"><input type="number" id="quantityValue" value="1" min="1"></div></td>' + '<td>' + price + '</td>' + '<td><button class="remove-product" onclick="removeL4D()">Remove</button></td>' + '</tr>';
-	
-		function removeL4D()
-		{
-			localStorage.removeItem("04Game");
-			localStorage.removeItem("04Price");
-			location.reload();
-		}
-	}
+//L4D
+if(L4D == "Left For Dead Two")
+{
+	let name = L4D;
+	customers.innerHTML+= '<tr>' + '<td><img id="thumbImages" src="GenreGames/Action/GameImages/L4D.jpg"></td>' + '<td>' + name +'</td>' + '<td>' + L4Dprice + '</td>' + '<td><div id="product-quantity"><input type="number" id="quantityValueL4D" value="1" min="1"></div> <button class="SaveButton" onclick="saveButtonL4D()">Save</button></td>' + '<td><button class="remove-product" onclick="removeL4D()">Remove</button></td>' + '<td id="totalPriceL4D">' + L4Dprice + '</td></tr>';
+}
+
+function removeL4D()
+{
+	localStorage.removeItem("04Game");
+	localStorage.removeItem("04Price");
+	location.reload();
+}
+
+function saveButtonL4D()
+{
+	let totalPriceContentL4D = document.getElementById("totalPriceL4D");
+	let x = document.getElementById("quantityValueL4D").value;
+	let totalPriceL4D = L4Dprice * x;
+	totalPriceContentL4D.textContent = totalPriceL4D;
+}
+//End of L4D
 	
 	if(warframe == "Warframe")
 	{
