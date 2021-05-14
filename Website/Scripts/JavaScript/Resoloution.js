05aadbc6e604607b615f674c3ed3a4c5c2220bd5
CianMcGriskin <81272459+CianMcGriskin@users.noreply.github.com>
2021-05-14 20:30:57 +0100

    Update Resoloution.js

--- Website/Scripts/JavaScript/Resoloution.js
+++ Website/Scripts/JavaScript/Resoloution.js
@@ -7,3 +7,7 @@ if(window.outerWidth == 1680 && window.outerHeight == 1050)
 {
 	document.body.style.zoom = 0.8;
 }
+
+if(screen.width == 1680 && screen.height == 1050){
+	document.body.style.zoom = 0.8;
+}
