bf7ed880f6f58a13488a1e706937c84de74bf2b8
CianMcGriskin <81272459+CianMcGriskin@users.noreply.github.com>
2021-05-14 20:27:33 +0100

    Resoloution

---
+++ Website/Scripts/JavaScript/Resoloution.js
@@ -0,0 +1,9 @@
+//(C.McGr) Used to fix screen resolution issues
+if(window.outerWidth == 1366 && window.outerHeight == 768){
+	document.body.style.zoom = 0.8;
+}
+
+if(window.outerWidth == 1680 && window.outerHeight == 1050)
+{
+	document.body.style.zoom = 0.8;
+}
